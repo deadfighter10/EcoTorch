9e61be7a6c6d440b651ff25faf0b7eb728e1d06c
David Leonard Nagy <sajtoskecske2006@gmail.com>
2026-02-17 16:39:27 -0600

    updated metadata

--- scr/ecotorch/__init__.py
+++ scr/ecotorch/__init__.py
@@ -3,9 +3,16 @@ A lightweight package to measure the ecological and financial effect of training
 """
 
 from .main import Tracker, evaluate, train
+from .datahandler import DataHandler
 
 __version__ = "0.0.1"
-__author__ = "Leo Nagy"
+__author__ = "David Leonard Nagy"
+__copyright__ = "Copyright 2026, David Leonard Nagy"
+__credits__ = ["David Leonard Nagy"]
+__license__ = "MIT"
+__maintainer__ = "David Leonard Nagy"
+__email__ = "nagy.david.leonard@gmail.com"
+__status__ = "Development"
 
 __all__ = [
     "Tracker",
