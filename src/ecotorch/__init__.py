88895e70b491e6eb10dc42b93f55dc66c8a09792
David Leonard Nagy <sajtoskecske2006@gmail.com>
2026-02-25 10:32:23 -0600

    new version

--- src/ecotorch/__init__.py
+++ src/ecotorch/__init__.py
@@ -6,7 +6,7 @@ from ._geolocator import get_ip, get_location
 from .core import EvalTracker, Mode, TrainTracker, evaluate, train
 from .datahandler import DataHandler
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __author__ = "David Leonard Nagy"
 __copyright__ = "Copyright 2026, David Leonard Nagy"
 __credits__ = ["David Leonard Nagy"]
